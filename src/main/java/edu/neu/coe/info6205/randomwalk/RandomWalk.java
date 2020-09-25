4fc1fc92995739147b1be7584a72983986a7f993
Kshitijverma <kshitij.verma20@gmail.com>
2020-09-25 18:08:19 -0400

    adding the randomwalk file as assignment 1

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -21,6 +21,8 @@ public class RandomWalk {
      */
     private void move(int dx, int dy) {
         // TO BE IMPLEMENTED
+    	x += dx;
+    	y += dy;
     }
 
     /**
@@ -30,6 +32,9 @@ public class RandomWalk {
      */
     private void randomWalk(int m) {
         // TO BE IMPLEMENTED
+    	for(int i =0; i<=m; i++) {
+    		randomMove();
+    	}
     }
 
     /**
@@ -49,6 +54,8 @@ public class RandomWalk {
      */
     public double distance() {
         // TO BE IMPLEMENTED
+    	double euclideanDistance = Math.sqrt((Math.pow(x, 2)+Math.pow(y,2)));
+    	return euclideanDistance;
     }
 
     /**
@@ -71,11 +78,16 @@ public class RandomWalk {
     public static void main(String[] args) {
         if (args.length == 0)
             throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
-        int m = Integer.parseInt(args[0]);
-        int n = 30;
-        if (args.length > 1) n = Integer.parseInt(args[1]);
-        double meanDistance = randomWalkMulti(m, n);
-        System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+        for(int i = 10; i<= 60; i+=10) {
+        	System.out.println("No. of steps: " + i);
+        	for(int j = 0; j < 10;j++) {
+		        int m = i;
+		        int n = 40;
+		        if (args.length > 1) n = Integer.parseInt(args[1]);
+		        double meanDistance = randomWalkMulti(m, n);
+		        System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+        	}
+        }
     }
 
 }
